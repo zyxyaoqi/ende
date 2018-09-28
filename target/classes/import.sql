1255d1fcf2a686f23cfb38609ec2ac9b15fec93b
123001830@qq.com <123001830@qq.com>
2018-09-28 19:33:05 +0800

    remove target dir

--- target/classes/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users(username, password, idcard) VALUES ('test1', '$2a$04$XeMj0iRbtMSHwFJg/B0j7u1WUL/9xWs5TdzDBQkoRnW8nodFekdKu', '150105198311261421')
-INSERT INTO users(username, password, idcard) VALUES ('test2', '$2a$04$XeMj0iRbtMSHwFJg/B0j7u1WUL/9xWs5TdzDBQkoRnW8nodFekdKu', '150105198311261422')
